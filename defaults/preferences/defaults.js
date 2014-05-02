bdb8ec5f74eb66f538cf0e701ba3e7e2d38140f4
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2014-05-02 19:24:44 -0300

    Alteração da nomenclatura da Vara

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -3,7 +3,7 @@ pref('eproc.v1.consulta_processo.enable', true);
 pref('eproc.v1.html_to_pdf.enable', true);
 pref('eproc.v1.secao', 'SEÇÃO JUDICIÁRIA DE SANTA CATARINA');
 pref('eproc.v1.subsecao', 'SUBSEÇÃO JUDICIÁRIA DE ITAJAÍ');
-pref('eproc.v1.vara', '1ª Vara Federal e Juizado Especial Federal Criminal');
+pref('eproc.v1.vara', '1ª Vara Federal');
 pref('eproc.v1.alteracao_assunto.enable', true);
 pref('eproc.v1.eproc.enable', true);
 pref('eproc.v2.enable', true);
