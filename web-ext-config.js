08df0644184eea09a5b01989c3ca39455da44278
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-09-20 04:01:09 -0300

    Preferências do navegador

--- web-ext-config.js
+++ web-ext-config.js
@@ -6,4 +6,10 @@ module.exports = {
 		'README.md',
 		'web-ext-config.js',
 	],
+	lint: { selfHosted: true },
+	run: {
+		browserConsole: true,
+		pref: ['browser.link.open_newwindow=3'],
+		startUrl: ['https://eproc.jfsc.jus.br/eprocV2/'],
+	},
 };
