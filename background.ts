1813fdbff956a945e0c6aa38ddc7b38b0469a82c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-09-26 17:35:31 -0300

    Exclusão de código não utilizado

--- background.ts
+++ background.ts
@@ -1,15 +1,5 @@
 browser.browserAction.onClicked.addListener(openOptionsPage);
 
-browser.runtime.onMessage.addListener((msg, info) => {
-	if (typeof msg === 'object' && msg !== null) {
-		switch (msg.type) {
-			case 'options':
-				return openOptionsPage();
-		}
-	}
-	console.log('Background script received:', msg, info);
-});
-
 function openOptionsPage() {
 	browser.runtime.openOptionsPage();
 }
